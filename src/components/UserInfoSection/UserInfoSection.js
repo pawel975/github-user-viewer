d0361f8f3dd0337cb53377beb01c21fd7423d454
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-08-14 18:56:26 +0200

    user info section update

--- src/components/UserInfoSection/UserInfoSection.js
+++ src/components/UserInfoSection/UserInfoSection.js
@@ -1,12 +1,22 @@
 import "../UserInfoSection/UserInfoSection.scss";
 
-const UserInfoSection = () => {
+const UserInfoSection = ({userData}) => {
+
+    const {avatar_url, login, name} = userData;
 
     return (
         <section 
             className="user-info-section"
             data-testid="user-info-section"
         >
+            <div className="avatar-wrapper">
+                <img src={avatar_url} alt="user avatar"/>
+            </div>
+
+            <div className="info-wrapper">
+                <header className="info-wrapper__name">{name}</header>
+                <p className="info-wrapper__login">{login}</p>
+            </div>
 
         </section>
     )
